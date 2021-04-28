49837db1a8b7dbe228a9905b5b8fa615a36ce54a
Tamires Santos <tfs.tamsantos@gmail.com>
2021-04-28 16:32:07 -0300

    Webpack Dev Server

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -11,9 +11,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
+      template: path.resolve(__dirname, 'public', 'index.html'),
     })
   ],
   module: {
